50507d77d1cb7e305e0b847958aabf0ce5372660
Ben Partridge <ben.partridge249@gmail.com>
2020-07-12 14:42:43 +1000

    Create prefabs and end of level zone

---
+++ Assets/Scripts/KOZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KOZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
+
+        EndLevel();
+    }
+
+    private void EndLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
