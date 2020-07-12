77ecbe8c77c480bf9ab38babe61fea5f4c8ff299
Ben Partridge <ben.partridge249@gmail.com>
2020-07-12 13:24:42 +1000

    Add spatula launching

---
+++ Assets/Scripts/SpatulaWeapon.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatulaWeapon : MonoBehaviour
+{
+
+    public float launchForce;
+    private bool notFlipping = true;
+
+    private List<GameObject> thingsOnMe;
+
+    private void Start()
+    {
+        thingsOnMe = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (notFlipping && Input.GetAxisRaw("Fire1") != 0)
+        {
+            StartCoroutine(FlipSpatula());
+        }
+    }
+
+    IEnumerator FlipSpatula()
+    {
+        notFlipping = false;
+        for (var i = 0; i < 30; i++)
+        {
+            transform.eulerAngles = new Vector3(0, 0, i * 3);
+            yield return null;
+        }
+
+        thingsOnMe.ForEach(gobj =>
+        {
+            var rb = gobj.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.AddForce(new Vector2(-1, 1) * launchForce, ForceMode2D.Impulse);
+            }
+        });
+
+        yield return new WaitForSeconds(1);
+
+        for (var i = 0; i < 90; i++)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90 - i);
+            yield return null;
+        }
+
+        notFlipping = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        thingsOnMe.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        thingsOnMe.Remove(other.gameObject);
+    }
+}
