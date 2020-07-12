233f576d04858972e8fc1aa2839fd0f260260e78
Ben Partridge <ben.partridge249@gmail.com>
2020-07-12 14:08:13 +1000

    Add blowtorch

---
+++ Assets/Scripts/Blowtorch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blowtorch : MonoBehaviour
+{
+    public GameObject flame;
+    public float power;
+    public Rigidbody2D attachedBody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Fire2") != 0)
+        {
+            flame.SetActive(true);
+            attachedBody.AddForce(transform.right.normalized * power);
+        }
+        else
+        {
+            flame.SetActive(false);
+        }
+    }
+}
