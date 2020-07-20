c656993ba77ad3880b02ef2d60a15d3c5c286fd4
Ben Partridge <ben.partridge249@gmail.com>
2020-07-20 20:10:32 +1000

    Wire up menu and equipping weapons in preview.

--- Assets/Scripts/ManagedWeaponsLoadout.cs
+++ Assets/Scripts/ManagedWeaponsLoadout.cs
@@ -8,10 +8,49 @@ public class ManagedWeaponsLoadout : MonoBehaviour
 
     private RobotConfiguration configuration;
 
+    private GameObject frontWeaponObject;
+    private GameObject backWeaponObject;
+
     void Start()
     {
+        configuration = GetComponent<RobotConfiguration>();
+        EquipWeapons();
+    }
+
+    public void RefreshLoadout()
+    {
+        EquipWeapons();
+    }
+
+    private void EquipWeapons()
+    {
+        if (frontWeaponObject != null)
+        {
+            frontWeaponObject.SetActive(false);
+        }
+        if (backWeaponObject != null)
+        {
+            backWeaponObject.SetActive(false);
+        }
+
         RobotWeaponsManager weaponsManager = RobotWeaponsManager.Instance;
-        GameObject frontWeapon = Instantiate(weaponsManager.FrontWeapon.prefab, configuration.FrontWeaponPosition.position, Quaternion.identity, transform);
-        GameObject backWeapon = Instantiate(weaponsManager.BackWeapon.prefab, configuration.BackWeaponPosition.position, Quaternion.identity, transform);
+        RobotWeapon frontWeapon = weaponsManager.FrontWeapon;
+        if (frontWeapon != null)
+        {
+            frontWeaponObject = Instantiate(weaponsManager.FrontWeapon.prefab, configuration.FrontWeaponPosition.position, Quaternion.identity, transform);
+        }
+        else
+        {
+            Debug.Log("no front weapon equipped, not instantiating");
+        }
+        RobotWeapon backWeapon = weaponsManager.BackWeapon;
+        if (backWeapon != null)
+        {
+            backWeaponObject = Instantiate(weaponsManager.BackWeapon.prefab, configuration.BackWeaponPosition.position, Quaternion.identity, transform);
+        }
+        else
+        {
+            Debug.Log("no back weapon equipped, not instantiating");
+        }
     }
 }
