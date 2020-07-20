5978bd6527a4f41e2e5271cb345bf28d36d7befe
Ben Partridge <ben.partridge249@gmail.com>
2020-07-20 19:51:47 +1000

    Add script for configuring how robot equips weapons

---
+++ Assets/Scripts/RobotConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotConfiguration : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _frontWeaponPosition;
+    [SerializeField]
+    private Transform _backWeaponPosition;
+
+    public Transform FrontWeaponPosition { get { return _frontWeaponPosition; } }
+    public Transform BackWeaponPosition { get { return _backWeaponPosition; } }
+}
