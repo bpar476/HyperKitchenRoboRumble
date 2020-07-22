cc83933c06948e21eff01bbbf01b9324df8913f9
Ben Partridge <ben.partridge249@gmail.com>
2020-07-23 07:48:31 +1000

    Create abstract class for using weapons

---
+++ Assets/Scripts/AbstractWeapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractWeapon : MonoBehaviour
+{
+    public abstract void Use();
+    public abstract void StopUsing();
+    public abstract void EquipToPlayer(GameObject player);
+
+}
